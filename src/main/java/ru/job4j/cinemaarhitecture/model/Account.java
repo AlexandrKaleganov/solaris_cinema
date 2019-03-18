66c02161a08ae4dc484ba025a3718be2d1b60f85
AlexandrKaleganov <alexmur07@mail.ru>
2019-03-18 10:27:30 +0700

    Добавлен класс аккаунт

--- src/main/java/ru/job4j/cinemaarhitecture/model/Account.java
+++ src/main/java/ru/job4j/cinemaarhitecture/model/Account.java
@@ -1,5 +1,61 @@
 package ru.job4j.cinemaarhitecture.model;
 
-public class Account {
+import java.util.Objects;
 
+public class Account {
+    private Integer id;
+    private String name;
+    private String tel;
+
+    public Account(Integer id, String name, String tel) {
+        this.id = id;
+        this.name = name;
+        this.tel = tel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Account account = (Account) o;
+        return Objects.equals(id, account.id) && Objects.equals(name, account.name) && Objects.equals(tel, account.tel);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "id=" + id + ", name='" + name + '\'' + ", tel='" + tel + '\'' + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, tel);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 }
---
+++ src/test/java/ru/job4j/cinemaarhitecture/model/AccountTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.cinemaarhitecture.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.StringWriter;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class AccountTest {
+    private Account account;
+    private Account account2;
+    private Account account3;
+
+
+    @Before
+    public void setUp() throws Exception {
+        this.account = new Account(1, "Sasha", "+79139007067");
+        this.account2 = new Account(1, "Vasia", "+79139007067");
+        this.account3 = new Account(1, "Sasha", "+79139007067");
+    }
+
+    @Test
+    public void equals() {
+        assertThat(account.equals(account3), is(true));
+        assertThat(account.equals(account2), is(false));
+
+    }
+
+    @Test
+    public void toStringg() {
+        StringWriter actual = new StringWriter();
+        StringWriter expected = new StringWriter();
+        actual.write(this.account.toString());
+        expected.write("Account{id=1, name='Sasha', tel='+79139007067'}");
+        Assert.assertThat(actual.toString(), is(expected.toString()));
+    }
+
+    @Test
+    public void hashCodee() {
+        assertThat(account.hashCode() == account2.hashCode(), is(false));
+        assertThat(account.hashCode() == account3.hashCode(), is(true));
+
+    }
+
+    @Test
+    public void getId() {
+        assertThat(account.getId(), is(1));
+    }
+
+    @Test
+    public void setId() {
+        account2.setId(8);
+        assertThat(account2.getId(), is(8));
+    }
+
+    @Test
+    public void getName() {
+        assertThat(account.getName(), is("Sasha"));
+    }
+
+    @Test
+    public void setName() {
+        account.setName("vasia");
+        assertThat(account.getName(), is("vasia"));
+    }
+
+    @Test
+    public void getTel() {
+        assertThat(account.getTel(), is("+79139007067"));
+    }
+
+    @Test
+    public void setTel() {
+        account.setTel("123");
+        assertThat(account.getTel(), is("123"));
+    }
+}
